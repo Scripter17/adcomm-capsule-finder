f2b12986a8f379657d6a9b6bda0a962de3a9a8f3
Scripter17 <jameschristopherwise@gmail.com>
2019-05-31 13:43:58 -0400

    Got the basics of the UI working
    Now to make the highlighting system...
    Ugh

--- main.js
+++ main.js
@@ -16,55 +16,73 @@ window.capNames={
 	}
 }
 window.currentType="main";
-window.currentSeq="";
+window.currentSeq={
+	"main":"",
+	"event":""
+};
 window.onload=function(){
 	init();
 }
 function setType(t){
-	if (window.currentType!=t){
-		window.currentType=t;
-		renderInit();
-	}
+	document.body.setAttribute("type", t);
+	localStorage.setItem("type", t);
+	window.currentType=t;
 }
 function init(){
-	window.addBox=document.getElementById("cap-add");
-	window.seqBox=document.getElementById("cap-seq");
-	window.probBox=document.getElementById("cap-prob");
-	window.saveBox=document.getElementById("cap-save");
-	window.loopBox=document.getElementById("cap-loop");
-	window.shareBox=document.getElementById("cap-share");
-
-	renderInit();
-}
-function renderInit(){
-	var x, i;
-
-	window.addBox.innerHTML="";
-	window.saveBox.innerHTML="";
-	window.loopBox.innerHTML="";
-	window.currentSeq="";
-
-	for (x in window.capNames[window.currentType]){
-		window.addBox.innerHTML+="<button class='cap cap-"+x+"' onclick='addCapsule(\""+x+"\")'></button>"
+	var x, i, w, e,
+		b=["add", "seq", "prob", "save", "loop", "share"];
+	for (w in window.loops){
+		e=document.getElementById("world-"+w);
+		e.innerHTML+='<div class="add-wrap" id="cap-add-'+w+'"></div>'
+		e.innerHTML+='<div class="seq-wrap" id="cap-seq-'+w+'"></div>'
+		e.innerHTML+='<div class="prob-wrap" id="cap-prob-'+w+'"></div>'
+		e.innerHTML+='<table class="save-wrap" id="cap-save-'+w+'"></table>'
+		e.innerHTML+='<input class="share-wrap" id="cap-share-'+w+'" type="text"/>'
+		e.innerHTML+='<hr/>'
+		e.innerHTML+='<div class="loop-wrap" id="cap-loop-'+w+'"></div>'
 	}
-	for (i=0; i<window.loops[window.currentType].length; i++){
-		x=window.loops[window.currentType][i];
-		window.loopBox.innerHTML+="<b class='cap cap-"+x+"'>"+(i+1)+"</b>"
+
+	for (i=0; i<b.length; i++){
+		window[b[i]+"Box"]={}
+		for (w in window.loops){
+			window[b[i]+"Box"][w]=document.getElementById("cap-"+b[i]+"-"+w);
+		}
 	}
+
+	if (localStorage.getItem("type")==null){
+		setType(window.currentType);
+	} else {
+		setType(localStorage.getItem("type"));
+	}
+
+	for (w in window.loops){
+		for (x in window.capNames[w]){
+			window.addBox[w].innerHTML+="<button class='cap cap-"+x+"' onclick='addCapsule(\""+x+"\", \""+w+"\")'></button>"
+		}
+		window.addBox[w].innerHTML+="<button class='cap cap-1' onclick=\"addCapsule('1', '"+w+"')\"></button>"
+		window.addBox[w].innerHTML+="<button class='cap cap-DEL' onclick=\"addCapsule('-', '"+w+"')\"></button>"
+		for (i=0; i<window.loops[w].length; i++){
+			x=window.loops[w][i];
+			window.loopBox[w].innerHTML+="<b class='cap cap-"+x+"'>"+(i+1)+"</b>"
+		}
+	}
+
 	render();
 }
-function addCapsule(t){
+function addCapsule(t, w){
 	if (t=="-"){
-		window.currentSeq=window.currentSeq.substr(1, window.currentSeq.length-1);
+		window.currentSeq[w]=window.currentSeq[w].substr(1, window.currentSeq[w].length-1);
 	} else {
-		window.currentSeq+=t;
+		window.currentSeq[w]+=t;
 	}
 	render();
 }
 function render(){
-	var i;
-	window.seqBox.innerHTML="";
-	for (i=0; i<window.currentSeq.length; i++){
-		window.seqBox.innerHTML+="<b class='cap cap-"+window.currentSeq[i]+"'></b>"
+	var i, w;
+	for (w in window.loops){
+		window.seqBox[w].innerHTML="";
+		for (i=0; i<window.currentSeq[w].length; i++){
+			window.seqBox[w].innerHTML+="<b class='cap cap-"+window.currentSeq[w][i]+"'></b>"
+		}
 	}
 }

2390db3862b50d9cfd370b61d43ad67390e2e396
Scripter17 <jameschristopherwise@gmail.com>
2019-06-04 14:51:33 -0400

    Good time to back this up

--- CardCalc/main.js
+++ CardCalc/main.js
@@ -7,87 +7,140 @@ async function main(){
 	var world, cworld,
 		indus, cindus,
 		build, cbuild,
-		attr, cattr,
 		card, ccard,
-		cattr, ccattr,
+		bname,
 		mainDiv=document.getElementById("main-div"),
-		wiHTML, wbHTML, ibHTML,
-		worldButtons, wbHTML,
-		indusButtons, inHTML,
-		cardWrap, cwHTML;
+		htmlTMP="",
+		boost,
+		boostType;
 	await fetch("/dat.json").then(res=>res.json()).then(res=>{window.dat=res;});
 
-	/*wiHTML="<table id='wi-button-wrap'>"
+	htmlTMP+="<table><tr>";
 	for (world in window.dat){
-		wiHTML+="<tr><td><table><tr><td><button onclick='setWorld(\""+world+"\")'>"+world+"</button></td></tr><tr>"
+		htmlTMP+="<td><button class='set-world' id='set-world-"+world+"'>"+world+"</button></td>";
+	}
+	htmlTMP+="</tr></table>";
+	for (world in window.dat){
+		htmlTMP+="<table class='set-indus-wrap' id='set-indus-"+world+"'><tr>"
 		for (indus in window.dat[world]){
-			wiHTML+="<td><button onclick='setIndus(\""+world+"\", \""+indus+"\")'>"+indus+"</button></td>"
+			htmlTMP+="<td><button class='set-indus' id='set-indus-"+world+"-"+indus+"'>"+indus+"</button></td>";
 		}
-		wiHTML+="</tr></table></td></tr>"
+		htmlTMP+="</tr></table>"
 	}
-	wiHTML+="</table>";*/
-	wiHTML="<table id='world-button-wrap'><tr>"
-	for (world in window.dat){
-		wiHTML+="<td><button onclick='setWorld(\""+world+"\")'>"+world+"</button></td>"
-	}
-	wiHTML+="</tr></table>"
-	for (world in window.dat){
-		wiHTML+="<table class='world-"+world+"' id='"+world+"-button-wrap'><tr>"
-		for (indus in window.dat[world]){
-			wiHTML+="<td><button onclick='setIndus(\""+world+"\", \""+indus+"\")'>"+indus+"</button></td>"
-		}
-		wiHTML+="</tr></table>"
-	}
-	mainDiv.innerHTML+=wiHTML
 
+	//boost=calcBoosts();
 	for (world in window.dat){
-		mainDiv.innerHTML+="<table id='cards-world-"+world+"'></table>"
-		cardWrap=document.getElementById("cards-world-"+world);
-		cwHTML=""
-		cworld=window.dat[world];
-		cwHTML+="<table id='cards-industry-"+world+"'><tr>" // Industry wrapper
-		// Industry cards (discount and stuff)
+		cworld=window.dat[world]
+		htmlTMP+="<table id='world-"+world+"'><tr>";
 		for (indus in cworld){
-			cindus=cworld[indus];
-			cwHTML+="<td>" // World type wrapper
+			cindus=cworld[indus]
+			htmlTMP+="<td id='indus-"+indus+"'>";
 			for (card in cindus["cards"]){
-				ccard=cindus["cards"][card];
-				cwHTML+="<table>" // Attribute wrapper
-				for (cattr in ccard){
-					ccattr=ccard[cattr];
-					cwHTML+="<tr><td>"+cattr+":"+ccattr+"</td></tr>"
-				}
-				cwHTML+="</table>" // Close attributes
+				ccard=cindus["cards"][card]
+				bm=0//boost[world][indus]["cards"][ccard["type"]]
+				beforeV=afterV="aa"
+				htmlTMP+="<table><tr><td>"+ccard["name"]+"</td></tr>"
+				htmlTMP+="<tr><td><input type='number' value='0' min='0' max='"+ccard["multiplier"].length+"' id='cardlev-"+world+"-"+indus+"-null-"+card+"'/></td></tr>"
+				htmlTMP+="</table>"
 			}
-			// Building specific cards
-			cwHTML+="<table><tr><td>"
 			for (build in cindus["buildings"]){
-				cbuild=cindus["buildings"][build];
-				cwHTML+="<table>" // Building wrapper
-				for (attr in cbuild){
-					cattr=cbuild[attr];
-					if (attr=="cards"){continue;}
-					//if (attr=="cost"){cattr=cattr["comrade"]+"c+"+cattr["indus"]+"i"+(cattr["lastTier"]!=0?"+"+cattr["lastTier"]+"t":"")}
-					cwHTML+="<tr><td>"+attr+":"+cattr+"</td></tr>"
-				}
-				cwHTML+="<tr><td><table>"
-				for (card in cbuild["cards"]){
-					ccard=cbuild["cards"][card]
-					for (attr in ccard){
-						cattr=ccard[attr]
-						cwHTML+="<tr><td>"+attr+":"+cattr+"</td></tr>"
+				noCard={"speed":0, "power":0, "chance":0, "boost":0, "discount":0};
+				cbuild=cindus["buildings"][build]
+				bname=cbuild["name"]
+				htmlTMP+="<table id='build-"+build+"'>";
+				htmlTMP+="<tr><td><img src='img/build"+build+".png' alt='"+bname+"'></td><td>"+bname+"</td></tr>"
+				for (boostType in noCard){
+					isCard=false;
+					for (card in cbuild["cards"]){
+						if (cbuild["cards"][card]["type"]==boostType){
+							isCard=true;
+							ccard=cbuild["cards"][card]
+						}
 					}
-					cwHTML+="<tr><td><input type='number' min='0' max='"+ccard["max-level"]+"'></input></td></tr>"
+					//bm=null//boost[world][indus][build][ccard["type"]]
+					bm="<span id='bm-"+world+"-"+indus+"-"+build+"-"+card+"'>null</span>"
+					if (isCard){
+						//ccard=cbuild["cards"][card];
+						//bm=boost[world][indus][build][ccard[type]];
+						//bm=555
+						//console.log(ccard)
+						beforeV=afterV="aa"
+						htmlTMP+="<tr><td><table><tr><td>"+ccard["name"]+"</td></tr>"
+						htmlTMP+="<tr><td><input type='number' value='0' min='0' max='"+ccard["multiplier"].length+"' id='cardlev-"+world+"-"+indus+"-"+build+"-"+card+"'/></td></tr></table></td>"
+						htmlTMP+="<td><table><tr><td><img src='img/type-"+ccard["type"]+".png' alt='"+boostType+"'></td><td>"+bm+"</td></tr>"
+						if (boostType=="speed" || boostType=="power" || boostType=="discount"){
+							htmlTMP+="<tr><td>"+beforeV+"</td><td>"+afterV+"</td></tr>"
+						}
+						htmlTMP+="</table>"
+					} else {
+						//bm=444
+						beforeV=afterV="bb"
+						htmlTMP+="<tr><td colspan='2'><table><tr><td><img src='img/type-"+boostType+".png' alt='"+boostType+"'></td><td>"+bm+"</td></tr>"
+						if (boostType=="speed" || boostType=="power" || boostType=="discount"){
+							htmlTMP+="<tr><td>"+beforeV+"</td><td>"+afterV+"</td></tr>"
+						}
+						htmlTMP+="</table>"
+					}
 				}
-				cwHTML+="</table></td></tr>"
-				cwHTML+="</table>"
+				htmlTMP+="</table>"
 			}
-			cwHTML+="</td></tr></table>"
 		}
-		cwHTML+="</tr></table>"
-		cardWrap.innerHTML=cwHTML;
+		htmlTMP+="</tr></table>";
 	}
-	cardWrap.innerHTML=cwHTML
+	mainDiv.innerHTML=htmlTMP;
+
+	writeVals();
+}
+function writeVals(){
+	
+}
+function getValue(world, indus, build, card){
+	return document.getElementById(world+"-"+indus+"-"+build+"-"+card).value
+}
+function stackValues(a, b, type){
+	return a+b;
+}
+function calcBoosts(){
+	var world, ret={};
+	for (world in window.dat){
+		// For every world
+		ret[world]={}
+		cworld=window.dat[world]
+		for (indus in cworld){
+			// For every industry
+			ret[world][indus]={}
+			cindus=cworld[indus]
+			for (build in cindus["buildings"]){
+				// For every building
+				ret[world][indus][build]={};
+				cbuild=cindus["buildings"][build]
+				for (card in cbuild["cards"]){
+					// For every card in the building
+					ccard=cbuild["cards"][card]
+					console.log(world, indus, build, ccard["type"], ret[world][indus][build][ccard["type"]])
+					if (ret[world][indus][build][ccard["type"]]==undefined){
+						// If the value doesn't exist, write it
+						ret[world][indus][build][ccard["type"]]=getValue(world, indus, build, card)
+					} else {
+						// Properly add values
+						a=ret[world][indus][build][ccard["type"]];
+						b=getValue(world, indus, build, card)
+						ret[world][indus][build][ccard["type"]]=stackValues(a, b, ccard["type"])
+					}
+					console.log(world, indus, build, ccard["type"], ret[world][indus][build][ccard["type"]])
+				}
+				for (card in cindus["cards"]){
+					ccard=cindus["cards"][card]
+//					for (build in cindus["buildings"]){
+						a=ret[world][indus][build][ccard["type"]];
+						b=getValue(world, indus, build, card)
+						ret[world][indus][build][ccard["type"]]=stackValues(a, b, ccard["type"])
+//					}
+				}
+			}
+		}
+	}
+	return ret
 }
 
 /*
